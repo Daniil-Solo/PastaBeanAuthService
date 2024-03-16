17003136354d0413df6a61ab8826e7b00c8f3a33
Daniil-Solo <daniil.solo1723@gmail.com>
2024-03-16 20:20:44 +0300

    add dto for service

---
+++ src/dto/signin.py
@@ -0,0 +1,18 @@
+import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class SignInCreateDTO(BaseModel):
+    user_agent: str
+    auth_token: str
+    user_id: int
+
+
+class SignDTO(BaseModel):
+    id: int
+    user_agent: str
+    auth_token: str
+    created_at: datetime.datetime
+    user_id: int
